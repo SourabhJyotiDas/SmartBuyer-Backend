61204122d42998e2b0b767a2ca2d5057299288ca
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-30 18:38:22 +0530

    Set Origin to Cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(fileUpload());
 app.use(
    cors({
       credentials: true,
-      origin: "http://localhost:3000",
+      origin: "https://smartbuyer.netlify.app",
       methods: ["GET", "POST", "PUT", "DELETE"],
    })
 );
